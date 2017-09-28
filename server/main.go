6d562c8b5a41cf7957e17c13c46ead4b61c102ac
Duc Nguyen <minhducng93@gmail.com>
2017-09-27 22:05:16 -0400

    Config Mongodb driver. Add pull data from Mongodb to API: /talents and /talent/{id}

--- server/main.go
+++ server/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
-
+	"time"
+	
 	"github.com/gorilla/mux"
+	mgo "gopkg.in/mgo.v2"
 )
 
-type User struct {
+type Talent struct {
 	ID       string
 	Username string
 	Fullname string
@@ -16,18 +20,35 @@ type User struct {
 	Avatar   string
 }
 
-var userList = []User{
-	User{ID: "1", Username: "jdoe", Fullname: "John Doe", Email: "jdoe@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
-	User{ID: "2", Username: "mprice", Fullname: "John Doe", Email: "mprice@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
-	User{ID: "3", Username: "dtow", Fullname: "John Doe", Email: "dtow@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
-	User{ID: "4", Username: "wjackson", Fullname: "John Doe", Email: "wjackson@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
-	User{ID: "5", Username: "tswift", Fullname: "John Doe", Email: "tswift@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
-	User{ID: "6", Username: "dpeter", Fullname: "John Doe", Email: "dpeter@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
+//const MongoDb details
+
+var info = &mgo.DialInfo{
+	Addrs:    []string{hosts},
+	Timeout:  60 * time.Second,
+	Database: database,
+	Username: username,
+	Password: password,
 }
 
+var session, errDial = mgo.DialWithInfo(info)
+var col = session.DB("stalents").C("talents")
+
 var GetTalentAllHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// Here we are converting the slice of products to json
-	payload, _ := json.Marshal(userList)
+
+	// Get all items in talent collection
+	if errDial != nil {
+		panic(errDial)
+	}
+
+	var results []Talent
+
+	err := col.Find(nil).All(&results)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Results All: ", results)
+	}
+	payload, _ := json.Marshal(results)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(payload))
@@ -41,19 +62,32 @@ var GetTalentAllHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.R
 // }
 
 var GetTalentIDHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	var user User
+	var talent Talent
 	vars := mux.Vars(r) // mux.Vars()
 	id := vars["id"]
 
-	for _, u := range userList {
+	if errDial != nil {
+		panic(errDial)
+	}
+
+	var results []Talent
+
+	err := col.Find(nil).All(&results)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Results All: ", results)
+	}
+
+	for _, u := range results {
 		if u.ID == id {
-			user = u
+			talent = u
 		}
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	if user.ID != "" {
-		payload, _ := json.Marshal(userList)
+	if talent.ID != "" {
+		payload, _ := json.Marshal(talent)
 		w.Write([]byte(payload))
 	} else {
 		w.Write([]byte("Talent Not Found"))
@@ -61,6 +95,33 @@ var GetTalentIDHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Re
 })
 
 func main() {
+
+	// initiate MongoDB connection
+
+	// clear all the talent records
+
+	col.RemoveAll(nil)
+
+	// Add new talents records
+
+	err := col.Insert(
+		&Talent{ID: "1", Username: "jdoe", Fullname: "John Doe", Email: "jdoe@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
+		&Talent{ID: "2", Username: "mprice", Fullname: "John Doe", Email: "mprice@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
+		&Talent{ID: "3", Username: "dtow", Fullname: "John Doe", Email: "dtow@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
+		&Talent{ID: "4", Username: "wjackson", Fullname: "John Doe", Email: "wjackson@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
+		&Talent{ID: "5", Username: "tswift", Fullname: "John Doe", Email: "tswift@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
+		&Talent{ID: "6", Username: "dpeter", Fullname: "John Doe", Email: "dpeter@example.com", Bio: "We shine together", Avatar: "https://google.ca"},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Count all items in talent collection
+	count, err2 := col.Count()
+	if err2 != nil {
+		panic(err2)
+	}
+	fmt.Println(fmt.Sprintf("Talents count: %d", count))
 
 	// new instance gorilla mux router
 	r := mux.NewRouter()
